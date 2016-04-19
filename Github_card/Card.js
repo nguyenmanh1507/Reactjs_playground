d305eb53acebacb42f5a1bd8ae682d6fe55413f4
nguyen manh <nguyenmanh1507@gmail.com>
2016-04-19 21:00:23 +0700

    prevent render when invalid username entered

--- Github_card/Card.js
+++ Github_card/Card.js
@@ -6,47 +6,51 @@ class Card extends React.Component {
     this.state = {};
   }
 
-  componentDidMount() {
-    console.log('did mount');
+  componentWillMount() {
     $.get(`https://api.github.com/users/${this.props.login}?access_token=4fe569e02ea39143663da766a7fd0930d619f44f`, data => {
-      console.log(data);
+      data.notFound = false;
       this.setState(data);
-      // console.log('success');
     })
     .fail(error => {
-      console.log(error.status, error.statusText);
+      this.setState({
+        notFound: true
+      });
     });
   }
 
   render() {
     let data = this.state;
 
-    return (
-      <div className="col-sm-4">
+    if (!this.state.notFound) {
+      return (
+        <div className="col-sm-4">
 
-        <div className="thumbnail">
-          <a href={data.html_url} target="_blank"><img src={data.avatar_url} alt={data.name} /></a>
-          <div className="caption">
-            <h3>{data.name}</h3>
-            <ul className="list-group">
-              <li className="list-group-item">
-                <span className="badge">{data.public_repos}</span>
-                Public repos
-              </li>
-              <li className="list-group-item">
-                <span className="badge">{data.followers}</span>
-                Followers
-              </li>
-              <li className="list-group-item">
-                <span className="badge">{data.following}</span>
-                Following
-              </li>
-            </ul>
+          <div className="thumbnail">
+            <a href={data.html_url} target="_blank"><img src={data.avatar_url} alt={data.name} /></a>
+            <div className="caption">
+              <h3>{data.name}</h3>
+              <ul className="list-group">
+                <li className="list-group-item">
+                  <span className="badge">{data.public_repos}</span>
+                  Public repos
+                </li>
+                <li className="list-group-item">
+                  <span className="badge">{data.followers}</span>
+                  Followers
+                </li>
+                <li className="list-group-item">
+                  <span className="badge">{data.following}</span>
+                  Following
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
 
-      </div>
-    );
+        </div>
+      );
+    } else {
+      return false;
+    }
   }
 }
 
