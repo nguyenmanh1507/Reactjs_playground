63bf6b1b06b5f112b2c4633b6d27e29358a23571
nguyenmanh1507 <nguyenmanh1507@gmail.com>
2016-04-28 15:35:26 +0700

    update className for component

--- Weather/src/components/weather.js
+++ Weather/src/components/weather.js
@@ -2,21 +2,22 @@ import React from 'react';
 
 class Weather extends React.Component {
   render() {
-    let main = this.props.list.main;
     return (
-      <div className="thumbnail">
-        <img src="http://placehold.it/300x300" alt="Weather Icon" />
-        <div className="caption">
-          <ul className="list-group">
-            <li className="list-group-item">
-              <span className="badge"><i className="fa fa-clock-o"></i></span>
-              {this.props.list.dt_txt}
-            </li>
-            <li className="list-group-item">
-              <span className="badge">&deg;C</span>
-              {main.temp}
-            </li>
-          </ul>
+      <div className="col-md-4">
+        <div className="thumbnail">
+          <img src="http://placehold.it/300x300" alt="Weather Icon" />
+          <div className="caption">
+            <ul className="list-group">
+              <li className="list-group-item">
+                <span className="badge"><i className="fa fa-clock-o"></i></span>
+                {this.props.dateTime}
+              </li>
+              <li className="list-group-item">
+                <span className="badge">&deg;C</span>
+                {this.props.temp}
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     );
