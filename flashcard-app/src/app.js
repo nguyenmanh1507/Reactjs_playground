1f6f13a86875dd1af9b1ccd268678cecc3704aaf
nguyenmanh1507 <nguyenmanh1507@gmail.com>
2016-05-04 11:24:25 +0700

    basic redux

--- flashcard-app/src/app.js
+++ flashcard-app/src/app.js
@@ -1,1 +1,33 @@
-console.log('Hello');
+const store = Redux.createStore((state, action) => {
+  switch (action.type) {
+    case 'ADD_CARD':
+      let newCard = Object.assign({}, action.data, {
+        score: 1,
+        id: +new Date
+      });
+
+      return Object.assign({}, state, {
+        cards: state.cards ? state.cards.concat([newCard]) : [newCard]
+      });
+
+    default:
+      return state || {};
+  }
+});
+
+store.subscribe(() => {
+  console.log(store.getState());;
+});
+
+store.dispatch({
+  type: 'ADD_CARD',
+  data: {
+    front: 'front',
+    back: 'back'
+  }
+});
+
+store.dispatch({
+  type: 'ADD_CARD',
+  data: {}
+});
