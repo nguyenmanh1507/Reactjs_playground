78108efb7f4eb916f233b0eb36a1897f1478d33c
nguyenmanh1507 <nguyenmanh1507@gmail.com>
2016-05-05 13:21:33 +0700

    3.2.Start_the_Sidebar

--- flashcard-app/src/app.js
+++ flashcard-app/src/app.js
@@ -17,19 +17,36 @@ const store = Redux.createStore(Redux.combineReducers({
   cards: cards
 }));
 
-store.subscribe(() => {
-  console.log(store.getState());;
-});
-
-store.dispatch({
-  type: 'ADD_CARD',
-  data: {
-    front: 'front',
-    back: 'back'
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        {this.props.children}
+      </div>
+    );
   }
-});
+}
 
-store.dispatch({
-  type: 'ADD_CARD',
-  data: {}
-});
+class Sidebar extends React.Component {
+  render() {
+    let props = this.props;
+    return (
+      <div className="sidebar">
+        <h2>All Decks</h2>
+        <ul>
+          {props.decks.map((deck, i) => {
+            <li key={i}>{deck.name}</li>
+          })}
+        </ul>
+        {props.addingDeck && <input ref="add" />}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App>
+    <Sidebar decks={[{name: 'Deck 1'}]} addingDeck={false} />
+  </App>,
+  document.getElementById('root')
+);
