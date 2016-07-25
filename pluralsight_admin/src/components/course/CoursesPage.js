2ed7bbfed7e7f60f14eeb39eba8de46d882a0dcd
manh <nguyenmanh1507@gmail.com>
2016-07-25 12:49:38 +0700

    first action

--- pluralsight_admin/src/components/course/CoursesPage.js
+++ pluralsight_admin/src/components/course/CoursesPage.js
@@ -1,10 +1,42 @@
 import React, {PropTypes} from 'react'
 
 class CoursesPage extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      course: {
+        title: ''
+      }
+    }
+
+    this.onTitleChange = this.onTitleChange.bind(this)
+    this.onClickSave = this.onClickSave.bind(this)
+  }
+
+  onTitleChange(event) {
+    const course = this.state.course
+    course.title = event.target.value
+    this.setState({
+      course: course
+    })
+  }
+
+  onClickSave() {
+    console.log(`Saving ${this.state.course.title}`)
+  }
+
   render() {
     return (
       <div>
         <h1>Courses Page</h1>
+        <h2>Add Course</h2>
+        <input
+          type="text"
+          onChange={this.onTitleChange}
+          value={this.state.course.title}
+        />
+        <button type="submit" onClick={this.onClickSave}>Save</button>
       </div>
     )
   }
