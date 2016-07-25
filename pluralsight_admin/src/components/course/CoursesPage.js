2ea6193a242ec4ed40d155252eb61a290dfe8bab
manh <nguyenmanh1507@gmail.com>
2016-07-25 13:43:01 +0700

    ep78

--- pluralsight_admin/src/components/course/CoursesPage.js
+++ pluralsight_admin/src/components/course/CoursesPage.js
@@ -1,4 +1,6 @@
 import React, {PropTypes} from 'react'
+import {connect} from 'react-redux'
+import * as courseActions from '../../actions/courseActions'
 
 class CoursesPage extends React.Component {
   constructor() {
@@ -23,13 +25,18 @@ class CoursesPage extends React.Component {
   }
 
   onClickSave() {
-    console.log(`Saving ${this.state.course.title}`)
+    this.props.dispatch(courseActions.createCourse(this.state.course))
+  }
+
+  courseRow(course, index) {
+    return <div key={index}>{course.title}</div>
   }
 
   render() {
     return (
       <div>
         <h1>Courses Page</h1>
+        {this.state.courses.map(this.courseRow)}
         <h2>Add Course</h2>
         <input
           type="text"
@@ -42,4 +49,15 @@ class CoursesPage extends React.Component {
   }
 }
 
-export default CoursesPage
+CoursesPage.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  courses: PropTypes.array.isRequired
+}
+
+function mapStateToProps(state, ownProps) {
+  return {
+    courses: state.courses
+  }
+}
+
+export default connect(mapStateToProps)(CoursesPage)
